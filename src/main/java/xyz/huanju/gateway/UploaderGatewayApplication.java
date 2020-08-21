9c4caba1f55c4542c0dd69be19b62a4fdfe08350
huanjuchen <huanju.chen@qq.com>
2020-08-22 02:49:35 +0800

    提交

---
+++ src/main/java/xyz/huanju/gateway/UploaderGatewayApplication.java
@@ -0,0 +1,16 @@
+package xyz.huanju.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HuanJu
+ */
+@SpringBootApplication
+public class UploaderGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploaderGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/huanjugateway/UploaderGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.huanjugateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploaderGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
